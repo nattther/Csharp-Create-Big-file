8bb88aaea06b2391581476e961ac76218a08684b
Nathan <nathan.tournant@gmail.com>
2024-04-22 16:59:10 +0200

    refactor: ajout d'un gitignore

--- obj/Debug/net8.0/Async.AssemblyInfo.cs
+++ obj/Debug/net8.0/Async.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Async")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964149024fe6c5b8d2cd026ae7e75980ebdbf566")]
 [assembly: System.Reflection.AssemblyProductAttribute("Async")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Async")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
