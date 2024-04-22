bf959cb7455476ee9ba7c49ac4682d1dc88ed99e
Nathan <nathan.tournant@gmail.com>
2024-04-22 17:19:49 +0200

    features : Ajout de la lecture en asynchrone de x fichier

--- Program.cs
+++ Program.cs
@@ -1,33 +1,64 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Threading.Tasks;
 
 internal class Program
 {
-    private async static Task Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        await createFile();
+
+        Task file1Task = createFile(1);
+        Task file2Task = createFile(2);
+        await Task.WhenAll(file1Task, file2Task);
+
+        Task readFile1Task = ReadFile(1);
+        Task readFile2Task = ReadFile(2);
+        await Task.WhenAll(readFile1Task, readFile2Task);
+
+        Console.WriteLine("Les deux fichiers sont bien lus.");
         Console.WriteLine("Continuation du traitement");
     }
 
-
-
-    private async static Task createFile()
+    private static async Task createFile(int a)
     {
-
-        Console.WriteLine("Début de la lecture du fichier");
-        using (FileStream fs = File.Create(@"C:\Users\Nathan-Efficom\Desktop\cours\algo\Async\test.txt", 1024)) { }
-        using (StreamWriter sw = File.CreateText(@"C:\Users\Nathan-Efficom\Desktop\cours\algo\Async\test.txt"))
+        Console.WriteLine($"Début de l'écriture du fichier {a}");
+        using (StreamWriter sw = File.CreateText($@"C:\Users\Nathan-Efficom\Desktop\cours\algo\Async\test{a}.txt"))
         {
-            for (int i = 0; i < 9990000; i++)
+            for (int i = 0; i < 99000; i++)
             {
                 await sw.WriteLineAsync($"Line {i}");
             }
         }
-        Console.WriteLine("Fin de l'écriture dans le fichier");
+        Console.WriteLine($"Fin de l'écriture dans le fichier {a}");
     }
 
+    private static async Task ReadFile(int a)
+    {
+        Console.WriteLine($"Début de lecture du fichier {a}");
+        using (FileStream fsSource = new FileStream($@"C:\Users\Nathan-Efficom\Desktop\cours\algo\Async\test{a}.txt", FileMode.Open, FileAccess.Read))
+        {
+            using (StreamReader reader = new StreamReader(fsSource, Encoding.UTF8))
+            {
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    DeuxOuPas(line);
+                }
+            }
+        }
+        Console.WriteLine($"Fin de la lecture du fichier {a}");
+    }
 
+    private static void DeuxOuPas(string text)
+    {
+        foreach (char c in text)
+        {
+            if (c == '2')
+            {
+                Console.WriteLine(text);
+
+            }
+        }
+    }
 }
-
--- obj/Debug/net8.0/Async.AssemblyInfo.cs
+++ obj/Debug/net8.0/Async.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Async")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+964149024fe6c5b8d2cd026ae7e75980ebdbf566")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb88aaea06b2391581476e961ac76218a08684b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Async")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Async")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
