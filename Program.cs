964149024fe6c5b8d2cd026ae7e75980ebdbf566
Nathan <nathan.tournant@gmail.com>
2024-04-22 15:59:41 +0200

    creation de fichier volumineux

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+internal class Program
+{
+    private async static Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        await createFile();
+        Console.WriteLine("Continuation du traitement");
+    }
+
+
+
+    private async static Task createFile()
+    {
+
+        Console.WriteLine("Début de la lecture du fichier");
+        using (FileStream fs = File.Create(@"C:\Users\Nathan-Efficom\Desktop\cours\algo\Async\test.txt", 1024)) { }
+        using (StreamWriter sw = File.CreateText(@"C:\Users\Nathan-Efficom\Desktop\cours\algo\Async\test.txt"))
+        {
+            for (int i = 0; i < 9990000; i++)
+            {
+                await sw.WriteLineAsync($"Line {i}");
+            }
+        }
+        Console.WriteLine("Fin de l'écriture dans le fichier");
+    }
+
+
+}
+
